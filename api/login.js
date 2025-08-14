10945da307f6e48a9d1a8ad16e0d0c2864fe303b
clena <storeclena@gmail.com>
2025-08-13 21:30:48 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -5,13 +5,25 @@ export default function handler(req, res) {
 
   const { usuario, senha } = req.body;
 
-  // Pegando credenciais do painel da Vercel (Configurar em Settings → Environment Variables)
-  const userEnv = process.env[`USER_${usuario.toUpperCase()}`];
-  const passEnv = process.env[`PASS_${usuario.toUpperCase()}`];
+  const credenciais = {
+    mercatto: process.env.USER_MERCATTO,
+    villa: process.env.USER_VILLA,
+    padaria: process.env.USER_PADARIA
+  };
+  const senhas = {
+    mercatto: process.env.PASS_MERCATTO,
+    villa: process.env.PASS_VILLA,
+    padaria: process.env.PASS_PADARIA
+  };
 
-  if (userEnv && passEnv && senha === passEnv) {
-    return res.status(200).json({ sucesso: true });
-  } else {
-    return res.status(401).json({ sucesso: false, mensagem: 'Usuário ou senha inválidos' });
+  if (credenciais[usuario.toLowerCase()] && senhas[usuario.toLowerCase()]) {
+    if (
+      usuario.toLowerCase() === credenciais[usuario.toLowerCase()] &&
+      senha === senhas[usuario.toLowerCase()]
+    ) {
+      return res.status(200).json({ sucesso: true });
+    }
   }
+
+  return res.status(401).json({ sucesso: false, mensagem: 'Usuário ou senha inválidos' });
 }
